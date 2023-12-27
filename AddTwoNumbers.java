c49a1346f1b72dbf713279f94698a2d0d25acfe2
Rubal Goyal <105172154+rubalgoyal@users.noreply.github.com>
2023-12-27 12:40:10 -0700

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -60,3 +60,39 @@ class Solution {
         return twoNumbers.next;
     }
 }
+
+// Alternate Approach
+
+import java.math.BigInteger;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode twoNumbers = new ListNode();
+        ListNode temp = twoNumbers;
+        int carryOver = 0;
+        while(l1 != null || l2 != null || carryOver > 0){
+            if(l1 != null){
+                carryOver += l1.val;
+                l1 = l1.next;
+            }
+            if( l2 != null){
+                carryOver += l2.val;
+                l2 = l2.next;
+            }  
+            temp.next = new ListNode(carryOver % 10);
+            temp = temp.next;
+            carryOver = carryOver / 10;
+        }
+        return twoNumbers.next;
+    }
+}
+
